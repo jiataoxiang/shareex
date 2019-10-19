b0c2364dd96c645bfc8bb246adec8189311ecfbd
Huakun Shen <shenhuakun@outlook.com>
2019-10-18 22:34:15 -0400

    create react app for phase 1

---
+++ react_app/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+import Home from "./components/Home.jsx";
+import Hometest from "./components/hometest.jsx";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+import NotFound404 from "./components/404";
+
+function App() {
+  return (
+    <div>
+      {/* <Hometest /> */}
+      <BrowserRouter>
+        <Switch>
+          {" "}
+          {/* Similar to a switch statement - shows the component depending on the URL path */}
+          {/* Each Route below shows a different component depending on the exact path in the URL  */}
+          <Route exact path="/" component={Hometest} />
+          <Route exact path="/home" component={Home} />
+          <Route component={NotFound404} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ react_app/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Navbar from "./Navbar.jsx";
+class Home extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="container">
+        <Navbar />
+      </div>
+    );
+  }
+}
+
+export default Home;
