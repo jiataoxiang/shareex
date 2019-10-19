194d10f658f82fa64c9502f70e54c77de51117b5
Huakun Shen <shenhuakun@outlook.com>
2019-10-18 22:46:17 -0400

    made 404 not found page

---
+++ phase1/react_app/src/components/404.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import "../stylesheets/404.css";
+import { Link } from "react-router-dom";
+class NotFound404 extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <link
+          href="https://fonts.googleapis.com/css?family=Montserrat:700,900"
+          rel="stylesheet"
+        />
+        <link type="text/css" rel="stylesheet" href="css/style.css" />
+        <div id="notfound">
+          <div class="notfound">
+            <div class="notfound-404">
+              <h1>404</h1>
+              <h2>Page not found</h2>
+            </div>
+            <a>
+              <Link to={"./home"}>Go to Home Page</Link>
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NotFound404;
