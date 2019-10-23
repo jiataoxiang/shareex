139a3f5b2b243e6509cf12dab92c3f7758666aa7
Huakun Shen <shenhuakun@outlook.com>
2019-10-23 01:52:39 -0400

    update bootstrap setting, local bootstrap works now, as long as our custom css file is imported after importing bootstrap, dark theme would work.

--- phase1/react_app/src/App.js
+++ phase1/react_app/src/App.js
@@ -1,17 +1,15 @@
 import React from "react";
-import "./App.css";
 import Home from "./components/Homepage/Home";
-
 import UserProfiling from "./components/UserProfiling/UserProfiling";
 import Login from "./components/Login/Login";
 import SignUp from "./components/Signup/SignUp";
 import NewPost from "./components/NewPost/NewPost";
-
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import NotFound404 from "./components/404";
 import SinglePost from "./components/SinglePost/SinglePost";
 import Navbar from "./components/Navbar";
-// import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.scss";
 
 function App() {
   return (
@@ -24,7 +22,6 @@ function App() {
           {/* Similar to a switch statement - shows the component depending on the URL path */}
           {/* Each Route below shows a different component depending on the exact path in the URL  */}
           <Route exact path="/" component={Home} />
-
           <Route exact path="/userprofiling" component={UserProfiling} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={SignUp} />
