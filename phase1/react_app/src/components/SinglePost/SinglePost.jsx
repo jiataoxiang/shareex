3d82d8dc9c98a7d9ae2b639d361ad1b6abfbd6ef
Xu Wang <xuxu.wang@mail.utoronto.ca>
2019-10-24 11:22:43 -0400

    working on comment part

--- phase1/react_app/src/components/SinglePost/SinglePost.jsx
+++ phase1/react_app/src/components/SinglePost/SinglePost.jsx
@@ -47,9 +47,22 @@ class SinglePost extends Component {
                             </article>
                         </section>
                     </div>
-                    <div id="comment">
-                        <div className="card-body"></div>
-                    </div>
+                    <section className="comment-part">
+                        <div className="write-comment">
+                            <img className="user-avatar" src={process.env.PUBLIC_URL + "./img/saitama.jpg"}></img>
+                            <div id="comment-box">
+                                <textarea placeholder="comment"></textarea>
+                                <div className="input-group-append" id="button-addon4">
+                                    <button className="btn btn-outline-secondary" type="button">Submit</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="comments">
+                            <div className="one-comment">
+                                First comment here. 
+                            </div>
+                        </div>
+                    </section>
                 </div>
             </div>
         );
