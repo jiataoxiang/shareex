29e1115c156c8099443134520b29db959559dc5f
Xu Wang <xuxu.wang@mail.utoronto.ca>
2019-10-23 22:23:38 -0400

    updated single_post and new_post

--- phase1/react_app/src/App.js
+++ phase1/react_app/src/App.js
@@ -20,7 +20,6 @@ function App() {
         <Navbar />
         <Switch>
           <Route exact path="/" component={Home} />
-
           <Route exact path="/userprofile" component={UserProfile} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={SignUp} />
--- phase1/react_app/src/components/SinglePost/SinglePost.jsx
+++ phase1/react_app/src/components/SinglePost/SinglePost.jsx
@@ -1,36 +1,62 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Navbar from "../Navbar.jsx";
-import "../../stylesheets/SinglePost.css";
+import "../../stylesheets/single_post.scss";
 
 class SinglePost extends Component {
-  state = {};
-  componentDidMount() {}
-  render() {
-    return (
-      <div>
-        <Navbar />
-        <div id={"brief"}>
-          {/*The title of the post*/}
-          <h2>Field Trip</h2>
-          <div>
-            <ul className="list-group list-group-flush" id={"description"}>
-              <li className="list-group-item">Author: Bill Gates</li>
-              <li className="list-group-item">Location: US</li>
-              <li className="list-group-item">Likes: 666</li>
-            </ul>
-          </div>
-        </div>
-        <div>
-          <div id={"content"}>
-            <textarea>Hello from the other side....</textarea>
-          </div>
-          <div id={"comment"}>
-            <div className="card-body"> </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+    state = {};
 
-export default SinglePost;
+    componentDidMount() {
+    }
+
+    render() {
+        return (
+            <div className="single-page">
+                <aside className="brief">
+                    <section className="info">
+                        <div>
+                            <ul className="list-group list-group-flush" id="description">
+                                <li className="list-group-item">Author: Bill Gates</li>
+                                <li className="list-group-item">Location: US</li>
+                            </ul>
+                        </div>
+                    </section>
+                </aside>
+                <div className="content-comment">
+                    <div id="content">
+                        <section className="the-post">
+                            <article className="article">
+                              <p>
+                                <h1>
+                                  Programmers can't feel cold.
+                                </h1>
+                              </p>
+                              <div className="counts">
+                                <span>Viewed: 999</span>
+                                <span>Liked: 999</span>
+                              </div>
+                              <img
+                                  src={process.env.PUBLIC_URL + "./img/saitama.jpg"}
+                                  className="content-img"
+                                  alt=""
+                              ></img>
+                              <p>Hello from the other side...
+                                  Hello from the other side....Hello from the other side...Hello from the
+                                  other side...Hello from the other side...Hello from the other side...</p>
+                              <p>Hello from the other side....</p>
+                              <p>Hello from the other side....</p>
+                              <p>Hello from the other side....</p>
+                              <p>Hello from the other side....</p>
+                              <p>Hello from the other side....</p>
+                            </article>
+                        </section>
+                    </div>
+                    <div id="comment">
+                        <div className="card-body"></div>
+                    </div>
+                </div>
+            </div>
+    );
+    }
+    }
+
+    export default SinglePost;
