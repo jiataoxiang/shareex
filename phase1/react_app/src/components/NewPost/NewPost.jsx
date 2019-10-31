904865c074296d6f6610d04c082112b372d6ec94
Xu Wang <xuxu.wang@mail.utoronto.ca>
2019-10-30 23:01:02 -0400

    update new_post

--- phase1/react_app/src/components/NewPost/NewPost.jsx
+++ phase1/react_app/src/components/NewPost/NewPost.jsx
@@ -1,19 +1,43 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "../../stylesheets/new_post.scss";
+// import ReactSummernote from 'react-summernote';
+// import 'react-summernote/dist/react-summernote.css'; // import styles
+// import 'react-summernote/lang/summernote-ru-RU'; // you can import any other locale
+
+// Import bootstrap(v3 or v4) dependencies
+// import 'bootstrap/js/modal';
+// import 'bootstrap/js/dropdown';
+// import 'bootstrap/js/tooltip';
+// import 'bootstrap/dist/css/bootstrap.css';
 
 class PostContent extends Component {
   state = {};
-  componentDidMount() {}
+
+  componentDidMount() {
+  }
+
+  onChange(content) {
+    console.log('onChange', content);
+  }
+
   render() {
     return (
       <div className="new-post-page">
+        {/*<Helmet>*/}
+        {/*  <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"/>*/}
+        {/*  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/css/bootstrap.min.css"/>*/}
+        {/*  <script type="text/javascript"*/}
+        {/*          src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min.js"/>*/}
+        {/*  <link href="summernote.css" rel="stylesheet"/>*/}
+        {/*  <script src="summernote.js"/>*/}
+        {/*</Helmet>*/}
         <div className="container">
           <div className="article">
             <h1 id="title">Start a New Post</h1>
 
             <div className="form-group">
               <p>Title:</p>
-              <input type="text" className="form-control" id="tile" />
+              <input type="text" className="form-control" id="tile"/>
             </div>
             <div className="form-group">
               <p htmlFor="content">Content:</p>
@@ -53,6 +77,27 @@ class PostContent extends Component {
           <button type="submit" className="btn btn-success btn-md btn-block" id="post-button">
             Post
           </button>
+          {/*<script>*/}
+          {/*  {$(document).ready(function() {$('#summernote').summernote();})}*/}
+          {/*</script>*/}
+          {/*<ReactSummernote*/}
+          {/*  value="Default value"*/}
+          {/*  options={{*/}
+          {/*    lang: 'ru-RU',*/}
+          {/*    height: 350,*/}
+          {/*    dialogsInBody: true,*/}
+          {/*    toolbar: [*/}
+          {/*      ['style', ['style']],*/}
+          {/*      ['font', ['bold', 'underline', 'clear']],*/}
+          {/*      ['fontname', ['fontname']],*/}
+          {/*      ['para', ['ul', 'ol', 'paragraph']],*/}
+          {/*      ['table', ['table']],*/}
+          {/*      ['insert', ['link', 'picture', 'video']],*/}
+          {/*      ['view', ['fullscreen', 'codeview']]*/}
+          {/*    ]*/}
+          {/*  }}*/}
+          {/*  onChange={this.onChange}*/}
+          {/*/>*/}
         </div>
       </div>
     );
