d6be50be2e88a3a2f96804fa74bb5bb13a1c8f90
Xu Wang <xuxu.wang@mail.utoronto.ca>
2019-11-02 00:39:30 -0400

    merge with master

--- phase1/react_app/src/App.js
+++ phase1/react_app/src/App.js
@@ -12,7 +12,7 @@ import Navbar from "./components/Navbar";
 import Tmp from "./components/Tmp";
 // import { rand_string } from "./lib/util";
 // import { uid } from "react-uid";
-import { Route, Switch, BrowserRouter } from "react-router-dom";
+import {Route, Switch, BrowserRouter} from "react-router-dom";
 import mock_data from "./mock_data";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.scss";
@@ -36,23 +36,25 @@ class App extends React.Component {
     });
   }
 
+
+
   render() {
     return (
       <div>
         {/* <Hometest /> */}
         <BrowserRouter>
-          <Navbar val={this.state} />
+          <Navbar val={this.state}/>
           <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/index.html" component={Home} />
-            <Route exact path="/userprofile" component={UserProfile} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={SignUp} />
-            <Route exact path="/new_post" component={NewPost} />
-            <Route exact path="/single_post" component={SinglePost} />
-            <Route exact path="/prof_setting" component={ProfSetting} />
-            <Route exact path="/tmp" component={Tmp} />
-            <Route component={NotFound404} />
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/index.html" component={Home}/>
+            <Route exact path="/userprofile" component={UserProfile}/>
+            <Route exact path="/login" component={Login}/>
+            <Route exact path="/signup" component={SignUp}/>
+            <Route exact path="/new_post" data component={NewPost}/>
+            <Route exact path="/single_post" component={SinglePost}/>
+            <Route exact path="/prof_setting" component={ProfSetting}/>
+            <Route exact path="/tmp" component={Tmp}/>
+            <Route component={NotFound404}/>
           </Switch>
         </BrowserRouter>
       </div>
