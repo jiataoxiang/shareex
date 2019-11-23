16c86ae9083b3a4678d47a9e874f3d9778c55f3d
jiatao <a389157511@gmail.com>
2019-11-23 18:35:08 -0500

    finish initialize server code for OtherProfile page, but need to be tested

--- phase2/client/src/components/UserProfile/OtherProfile.jsx
+++ phase2/client/src/components/UserProfile/OtherProfile.jsx
@@ -4,9 +4,12 @@ import { withRouter} from "react-router-dom";
 import Post from "../Post";
 import { rand_string } from "../../lib/util";
 import { uid } from "react-uid";
+import axios from "axios"
+import { connect } from 'react-redux';
 
 class OtherProfile extends React.Component {
 
+  // TODO: NEED TO BE TESTED AFTER SINGLE POST FINISH.
   // TODO: connect to server, get information
   state = {
     nickname: "",
@@ -35,79 +38,100 @@ class OtherProfile extends React.Component {
       this.props.history.push("/");
     }
   }
+  componentWillMount() {
+    axios.get(`/api/posts/${this.state.post_id}`, this.props.tokenConfig())
+      .then((post) => {
+        this.setState({
+          post: post.data,
+          author: post.data.author
+        })
+      });
+  }
 
   componentDidMount() {
     this.loadUserFromServer();
-  } 
+  }
+
+  getNumPosts = (currentUser) => {
+    axios.get(`/api/posts/user-posts/${currentUser._id.toString()}`,  this.props.tokenConfig())
+      .then(posts => {
+        this.setState({
+          numPosts: posts.data.length
+        })
+      }).catch((error) => {
+      console.log(error)
+    })
+  };
+
+  getUserInfo = ()=>{
+    axios.get(`/api/users/${this.props.post.author}`, this.props.tokenConfig())
+      .then((user) => {
+        user = user.data;
+        this.setState({
+          nickname: user.nickname,
+          banner: user.banner,
+          avatar: user.avatar,
+          followers: user.followers.length,
+          following: user.following.length,
+          likes: user.likes.length,
+          motto: user.motto
+        })
+      })
+  };
 
   loadUserFromServer() {
     // Loading user from our fake data
     // TODO: replace the following initialization code in phase 2, connect to server and get real data
     // Current user info
-    const users = this.props.state.users;
-    const posts = this.props.state.posts;
-    if (posts) {
-      const post = posts.filter(post => post.id === this.state.post_id)[0];
-      const currentUser = users.filter(user => user.id === post.author_id)[0];
-      if (currentUser) {
-        this.setState({
-          nickname: currentUser.nickname,
-          banner: currentUser.banner,
-          avatar: currentUser.avatar,
-          follower: currentUser.follower,
-          following: currentUser.following,
-          likes: currentUser.likes,
-          numPosts: currentUser.numPosts,
-          motto: currentUser.motto
-        });
-        console.log(currentUser);
-      }
-    }
+    this.getUserInfo();
+    this.getNumPosts(this.props.author);
   }
 
-  getPosts = () => {
-    // TODO: connect to server, get posts from server
-    // find all posts belonging to current user
-    const users = this.props.state.users;
-    const posts = this.props.state.posts;
-    if (posts) {
-      const post = posts.filter(post => post.id === this.state.post_id)[0];
-      const current_user = users.filter(user => user.id === post.author_id)[0];
-      const posts_display = [];
-      if (this.props.state.posts) {
-        const posts = this.props.state.posts.filter(
-          post => post.author_id === current_user.id
-        );
-
-        if (posts) {
-          console.log(posts);
-          for (let i = 0; i < posts.length; i++) {
-            // find all attachments
-            const attachments = this.props.state.attachments.filter(
-              attachment => attachment.post_id === posts[i].id
-            );
-            posts_display.push(
-              <Post
-                key={uid(rand_string())}
-                post={posts[i]}
-                posts={posts}
-                users={this.props.state.users}
-                attachments={attachments}
-                current_user={this.props.state.current_user}
-                setAppState={this.props.state.setAppState}
-              />
-            );
-          }
-        }
-      }
-      return posts_display;
-    }else {
-      return [];
-    }
-  };
+  // getPosts = () => {
+  //   // TODO: connect to server, get posts from server
+  //   // find all posts belonging to current user
+  //   const users = this.props.state.users;
+  //   const posts = this.props.state.posts;
+  //   if (posts) {
+  //     const post = posts.filter(post => post.id === this.state.post_id)[0];
+  //     const current_user = users.filter(user => user.id === post.author_id)[0];
+  //     const posts_display = [];
+  //     if (this.props.state.posts) {
+  //       const posts = this.props.state.posts.filter(
+  //         post => post.author_id === current_user.id
+  //       );
+  //
+  //       if (posts) {
+  //         console.log(posts);
+  //         for (let i = 0; i < posts.length; i++) {
+  //           // find all attachments
+  //           const attachments = this.props.state.attachments.filter(
+  //             attachment => attachment.post_id === posts[i].id
+  //           );
+  //           posts_display.push(
+  //             <Post
+  //               key={uid(rand_string())}
+  //               post={posts[i]}
+  //               posts={posts}
+  //               users={this.props.state.users}
+  //               attachments={attachments}
+  //               current_user={this.props.state.current_user}
+  //               setAppState={this.props.state.setAppState}
+  //             />
+  //           );
+  //         }
+  //       }
+  //     }
+  //     return posts_display;
+  //   }else {
+  //     return [];
+  //   }
+  // };
 
   render() {
-
+    if(!this.props.isAuthenticated){
+      window.location.href = "/"
+    }
     return (
       <div className="user-profile-page">
         <img
@@ -176,4 +200,22 @@ class OtherProfile extends React.Component {
   }
 }
 
-export default withRouter(OtherProfile);
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated,
+  error: state.error,
+  current_user: state.auth.user,
+  tokenConfig: () => {
+    const config = {
+      headers: {
+        'Content-type': 'application/json'
+      }
+    };
+    // If token, add to headers
+    if (state.auth.token) {
+      config.headers['x-auth-token'] = state.auth.token;
+    }
+    return config
+  }
+});
+
+export default connect(mapStateToProps)(withRouter(OtherProfile));
