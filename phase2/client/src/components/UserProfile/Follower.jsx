16948dad59020d36519d90187d8a21e93c170d5e
jiatao <a389157511@gmail.com>
2019-11-24 15:55:00 -0500

    styling the follower display

--- phase2/client/src/components/UserProfile/Follower.jsx
+++ phase2/client/src/components/UserProfile/Follower.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from "axios";
 import '../../stylesheets/follower.scss';
+import { connect } from 'react-redux';
 
 class Follower extends React.Component {
   _isMount = false;
@@ -12,10 +13,11 @@ class Follower extends React.Component {
   componentDidMount() {
     this._isMount = true;
     //get follower avatar and name
-    axios.get(`/api/users/${this.state.follower}`)
+    axios.get(`/api/users/${this.state.follower}`, this.props.tokenConfig())
       .then((follower) => {
         if(this._isMount){
           this.setState({
+            follower_id : follower.data._id,
             follower_name: follower.data.username,
             follower_avatar: follower.data.avatar
           })
@@ -23,20 +25,42 @@ class Follower extends React.Component {
       }).catch((error) => {
         console.log(error)
     })
-  }
+  };
 
   componentWillUnmount() {
     this._isMount = false;
-  }
+  };
+
+  redirectProfile = () =>{
+
+  };
 
   render() {
     return (
       <div className="follower">
-        <img src={this.state.follower_avatar} className="avatar-image" alt="avatar"/>
+        <img src={this.state.follower_avatar} className="avatar-image" alt="avatar" onClick={this.redirectProfile}/>
         <span className="username-text">{this.state.follower_name}</span>
       </div>
     );
   }
 }
 
-export default Follower;
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated,
+  error: state.error,
+  current_user: state.auth.user,
+  tokenConfig: () => {
+    const config = {
+      headers: {
+        'Content-type': 'application/json'
+      }
+    };
+    // If token, add to headers
+    if (state.auth.token) {
+      config.headers['x-auth-token'] = state.auth.token;
+    }
+    return config;
+  }
+});
+
+export default connect(mapStateToProps)(Follower);
