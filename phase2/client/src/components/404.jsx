824511958070fcdca66564d2012f6dbf2fe2286d
Huakun Shen <shenhuakun@outlook.com>
2019-11-29 02:47:47 -0500

    Revert "re made 404 page"
    This reverts commit 296677db22f7102d16d77bf66a40d935e90d6450.

--- phase2/client/src/components/404.jsx
+++ phase2/client/src/components/404.jsx
@@ -1,29 +1,27 @@
-import React, { Component } from 'react';
-import '../stylesheets/404.css';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import "../stylesheets/404.css";
+import { Link } from "react-router-dom";
 class NotFound404 extends Component {
-  state = {};
-  render() {
-    return (
-      <div>
-        <link
-          href="https://fonts.googleapis.com/css?family=Montserrat:700,900"
-          rel="stylesheet"
-        />
-        <div id="notfound" className="container text-center">
-          <div className="notfound">
-            <div className="notfound-404">
-              <h1 className="num404">404</h1>
-              <h2>Page not found</h2>
+    state = {};
+    render() {
+        return (
+            <div>
+                <link
+                    href="https://fonts.googleapis.com/css?family=Montserrat:700,900"
+                    rel="stylesheet"
+                />
+                <div id="notfound">
+                    <div className="notfound">
+                        <div className="notfound-404">
+                            <h1>404</h1>
+                            <h2>Page not found</h2>
+                        </div>
+                        <Link to={"./"}>Go to Home Page</Link>
+                    </div>
+                </div>
             </div>
-            <Link className="home-link btn btn-danger" to={'./'}>
-              Go to Home Page
-            </Link>
-          </div>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default NotFound404;
