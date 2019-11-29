fa2ef9cc64b0f6229f165b4d83d39151bf78ccb3
Huakun Shen <shenhuakun@outlook.com>
2019-11-29 04:09:18 -0500

    redo 404 page

--- phase2/client/src/components/404.jsx
+++ phase2/client/src/components/404.jsx
@@ -1,27 +1,23 @@
-import React, { Component } from "react";
-import "../stylesheets/404.css";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import '../stylesheets/404.css';
+import { Link } from 'react-router-dom';
 class NotFound404 extends Component {
-    state = {};
-    render() {
-        return (
-            <div>
-                <link
-                    href="https://fonts.googleapis.com/css?family=Montserrat:700,900"
-                    rel="stylesheet"
-                />
-                <div id="notfound">
-                    <div className="notfound">
-                        <div className="notfound-404">
-                            <h1>404</h1>
-                            <h2>Page not found</h2>
-                        </div>
-                        <Link to={"./"}>Go to Home Page</Link>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  state = {};
+  render() {
+    return (
+      <div className="not-found-page">
+        <div className="notfound-container">
+          <div className="notfound-main">
+            <h1>404</h1>
+            <h2>Page not found</h2>
+          </div>
+          <Link className="btn btn-danger btn-lg home-btn" to={'/'}>
+            <strong>Go to Home Page</strong>
+          </Link>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default NotFound404;
